a13514a8d950584d5a51dfcaaada32b5c611400b
stAndrei <st.andreii@gmail.com>
2013-08-09 18:04:04 +0400

    Added status url to params

--- lib/active_merchant/billing/integrations/fasapay/helper.rb
+++ lib/active_merchant/billing/integrations/fasapay/helper.rb
@@ -9,9 +9,11 @@ module ActiveMerchant #:nodoc:
           mapping :description, 'fp_item'
           mapping :currency, 'fp_currency'
           mapping :transaction_type, 'fp_fee_mode'
+          mapping :status_url, 'fp_status_url'
           mapping :success_url, 'fp_success_url'
-          mapping :success_method, 'fp_success_method'
           mapping :fail_url, 'fp_fail_url'
+          mapping :status_method, 'fp_status_method'
+          mapping :success_method, 'fp_success_method'
           mapping :fail_method, 'fp_fail_method'
           mapping :order, 'fp_merchant_ref'
         end

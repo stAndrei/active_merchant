ee44cd4cb044f78ad7e33f907e7b1abbb10b1585
Elena <eleuna@yandex.ru>
2013-09-17 15:13:14 +0400

    before pull

--- lib/active_merchant/billing/integrations/fasapay/notification.rb
+++ lib/active_merchant/billing/integrations/fasapay/notification.rb
@@ -16,52 +16,52 @@ module ActiveMerchant #:nodoc:
             fp_store
             fp_timestamp
             fp_merchant_ref
-          ).each do |param_name|
-            define_method(param_name.underscore){ params[param_name] }
-          end
+            ).each do |param_name|
+              define_method(param_name.underscore){ params[param_name] }
+            end
 
-          alias_method :account, :fp_paidto
-          alias_method :payer, :fp_paidby
-          alias_method :amount, :fp_amnt
-          alias_method :fee_amount, :fp_fee_amnt
-          alias_method :currency, :fp_currency
-          alias_method :description, :fp_item
-          alias_method :account_name, :fp_store
-          alias_method :received_at, :fp_timestamp
-          alias_method :order, :fp_merchant_ref
-          alias_method :number, :fp_batchnumber
+            alias_method :account, :fp_paidto
+            alias_method :payer, :fp_paidby
+            alias_method :amount, :fp_amnt
+            alias_method :fee_amount, :fp_fee_amnt
+            alias_method :currency, :fp_currency
+            alias_method :description, :fp_item
+            alias_method :account_name, :fp_store
+            alias_method :received_at, :fp_timestamp
+            alias_method :order, :fp_merchant_ref
+            alias_method :number, :fp_batchnumber
 
-          def security_key
-            params["fp_hash"]
-          end
+            def security_key
+              params["fp_hash"]
+            end
 
-          def item_id
-            params['fp_merchant_ref']
-          end
+            def item_id
+              params['fp_merchant_ref']
+            end
 
-          def secret
-            @options[:secret]
-          end
+            def secret
+              @options[:secret]
+            end
 
-          def acknowledge
-            (security_key == generate_signature)
-          end
+            def acknowledge
+              (security_key == generate_signature)
+            end
 
-          def generate_signature_string
-            string = [
-                account,
-                payer,
-                account_name,
-                amount,
-                number,
-                currency,
-                @options[:secret]
-              ].join ':'
-          end
+            def generate_signature_string
+              string = [
+                  account,
+                  payer,
+                  account_name,
+                  amount,
+                  number,
+                  currency,
+                  @options[:secret]
+                ].join ':'
+            end
 
-          def generate_signature
-            Digest::SHA256.hexdigest(generate_signature_string).downcase
-          end
+            def generate_signature
+              Digest::SHA256.hexdigest(generate_signature_string).downcase
+            end
 
 
         end

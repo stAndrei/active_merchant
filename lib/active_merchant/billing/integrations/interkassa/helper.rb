443d8f1778a3e18b355da9175770122c9ccb3d3a
Alex <minax92@mail.ru>
2013-12-09 15:30:00 +0400

    add payment_id to params

--- lib/active_merchant/billing/integrations/interkassa/helper.rb
+++ lib/active_merchant/billing/integrations/interkassa/helper.rb
@@ -5,6 +5,7 @@ module ActiveMerchant #:nodoc:
         class Helper < ActiveMerchant::Billing::Integrations::Helper
 
           mapping :account, 'ik_co_id'
+          mapping :order, 'ik_pm_no'
           mapping :amount, 'ik_am'
           mapping :description, 'ik_desc'
         end

46415ed0a6708779807785c113406e3812afa8d6
Alex <minax92@mail.ru>
2013-12-09 16:26:17 +0400

    add params for yandex.dengi

--- lib/active_merchant/billing/integrations/interkassa/helper.rb
+++ lib/active_merchant/billing/integrations/interkassa/helper.rb
@@ -8,6 +8,9 @@ module ActiveMerchant #:nodoc:
           mapping :order, 'ik_pm_no'
           mapping :amount, 'ik_am'
           mapping :description, 'ik_desc'
+          mapping :action, 'ik_act'
+          mapping :payment_alias, 'ik_pw_via'
+          mapping :currency, 'ik_cur'
         end
       end
     end

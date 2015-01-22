510a98d105c8d4fb9236faf40a4341e9eabfc664
lesnka <eleuna@yandex.ru>
2015-01-22 18:40:27 +0300

    fixed ecommpay notification

--- lib/active_merchant/billing/integrations/ecommpay/notification.rb
+++ lib/active_merchant/billing/integrations/ecommpay/notification.rb
@@ -6,23 +6,48 @@ module ActiveMerchant #:nodoc:
       module Ecommpay
         class Notification < ActiveMerchant::Billing::Integrations::Notification
 
-          %w(
-            payment_type_id
-            transaction_id
-            external_id
-            site_id
-            order_description
-            currency
-            real_amount
-            real_currency
-            language
-            type
-            signature
-          ).each do |param_name|
+          %w( type_id
+              status_id
+              transaction_id
+              external_id
+              acquirer_id
+              payment_type_id
+              site_id
+              amount
+              currency
+              real_amount
+              real_currency
+              email
+              extended_info_enabled
+              customer_purse
+              completed_at
+              processor_date
+              source_type
+              holder_name
+              expiry_date
+              phone
+              authcode
+              billing_country
+              billing_region
+              billing_city
+              billing_address
+              billing_postal
+              parent_transaction_id
+              recurring_allowed
+              recurring_valid_thru
+              recurring_registration_id
+              processor_id
+              processor_code
+              processor_message
+              signature).each do |param_name|
             define_method(param_name.underscore){ params[param_name] }
           end
 
           alias_method :order, :external_id
+
+          def params_select
+            params.except('signature').map{|k, v| "#{k}:#{v}" if v.present? && v != 'NULL'}.compact
+          end
 
           def amount
             params['amount'].to_i / 100.0
@@ -33,7 +58,7 @@ module ActiveMerchant #:nodoc:
           end
 
           def generate_signature_string
-            params.map{|k, v| "#{k}:#{v}" }.sort.push(@options[:secret]).join(';')
+            params_select.sort.push(@options[:secret]).join(';')
           end
 
           def generate_signature

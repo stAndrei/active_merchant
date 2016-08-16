c506b379eff849938759104a91c10151f6184f95
AlexMinax <minax92@mail.ru>
2016-08-16 11:25:02 +0300

    BAC-852 add notification classes/helpers for cardpay

---
+++ lib/active_merchant/billing/integrations/cardpay/notification.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    module Integrations #:nodoc:
+      module Cardpay
+        class Notification < ActiveMerchant::Billing::Integrations::Notification
+
+        end
+      end
+    end
+  end
+end

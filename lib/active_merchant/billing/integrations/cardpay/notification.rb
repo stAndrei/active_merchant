a31b993b1e9630f2a8ab18151bfe4708b3b4d5ef
AlexMinax <minax92@mail.ru>
2016-08-17 18:01:03 +0300

    BAC-852 add cardpay notification methods

--- lib/active_merchant/billing/integrations/cardpay/notification.rb
+++ lib/active_merchant/billing/integrations/cardpay/notification.rb
@@ -6,6 +6,49 @@ module ActiveMerchant #:nodoc:
       module Cardpay
         class Notification < ActiveMerchant::Billing::Integrations::Notification
 
+          %w(orderXML sha512).each do |param_name|
+            define_method(param_name.underscore){ params[param_name] }
+          end
+
+          alias_method :signature, :sha512
+
+          def payment_params
+            Hash.from_xml(request_xml)["order"]
+          end
+
+          def request_xml
+            Base64.decode64(order_xml)
+          end
+
+          def status
+            case payment_params["status"]
+              when 'APPROVED' then 'completed'
+              when 'DECLINED' then 'canceled'
+              when 'PENDING' then 'pending'
+              else 'unknown'
+            end
+          end
+
+          def amount
+            payment_params["amount"].to_f
+          end
+
+          def currency
+            payment_params["currency"]
+          end
+
+          def payment_id
+            payment_params["number"].to_i
+          end
+
+          def generate_signature
+            Digest::SHA512.hexdigest(request_xml + @options[:secret])
+          end
+
+          def acknowledge
+            signature = generate_signature
+          end
+
         end
       end
     end

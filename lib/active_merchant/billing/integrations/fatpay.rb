f1441bff74d67230d232b1e42723c6a0142fb79b
AlexMinax <minax92@mail.ru>
2017-01-09 17:20:58 +0300

    BAC-980 change fatpay sevice_url from test to production

--- lib/active_merchant/billing/integrations/fatpay.rb
+++ lib/active_merchant/billing/integrations/fatpay.rb
@@ -6,7 +6,7 @@ module ActiveMerchant #:nodoc:
     module Integrations #:nodoc:
       module Fatpay
         mattr_accessor :service_url
-        self.service_url = 'https://fp.ki-technology.ru/pay'
+        self.service_url = 'https://fatpay.net/pay'
 
         def self.helper(order, account, options = {})
           Helper.new(order, account, options)

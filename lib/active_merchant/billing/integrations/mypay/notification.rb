4b06216e9315d2702cf4f91f7f4cfbdcb7bccec6
AlexMinax <minax92@mail.ru>
2017-04-07 14:40:45 +0300

    BAC-1033 change test url, add signature checking

--- lib/active_merchant/billing/integrations/mypay/notification.rb
+++ lib/active_merchant/billing/integrations/mypay/notification.rb
@@ -6,24 +6,50 @@ module ActiveMerchant #:nodoc:
       module Mypay
         class Notification < ActiveMerchant::Billing::Integrations::Notification
           %w(
-            clientid
+            id
             account
-            priceid
-            serviceid
-            amount
-            lang
-            successurl
-            failureurl
-            params
-            orderid
+            clientID
+            currency
+            date
+            error
+            errorMessage
+            parameters
+            paymentType
+            price
+            revenue
+            serviceID
+            status
+            timestamp
+            unitName
+            hash
           ).each do |param_name|
             define_method(param_name.underscore){ params[param_name] }
           end
 
           def acknowledge(authcode = nil)
-            true
+            string = [
+              account,
+              clientID,
+              currency,
+              date,
+              id,
+              orderId,
+              error,
+              errorMessage,
+              parameters,
+              paymentType,
+              price,
+              revenue,
+              serviceID,
+              status,
+              timestamp,
+              unitName,
+              @options[:secret]
+            ].join('')
+            signature = Digest::SHA1.hexdigest(string)
+            signature == hash
           end
-          
+
         end
       end
     end

1d1e69f3f8f3566f244834a6b80a7925afbfeedc
AlexMinax <minax92@mail.ru>
2017-04-14 12:40:49 +0300

    update MyPay service_url(at fact - not used)

--- lib/active_merchant/billing/integrations/mypay.rb
+++ lib/active_merchant/billing/integrations/mypay.rb
@@ -7,7 +7,7 @@ module ActiveMerchant #:nodoc:
       module Mypay
 
         mattr_accessor :service_url
-        self.service_url = 'http://202.60.204.148:8113/api/payment/initializepayment'
+        self.service_url = 'https://api.mypaymobi.com/api/payment/initializepayment'
 
         def self.helper(order, account, options = {})
           Helper.new(order, account, options)
